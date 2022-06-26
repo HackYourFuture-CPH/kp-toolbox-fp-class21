ae0d50046c9e22b6391eff3b6f2b162f8266f54c
rismita87 <rismita.ghosh@gmail.com>
2022-06-26 16:01:03 +0200

    code added FP21-12-welcome-box-ImagePlaceHolder

---
+++ packages/client/src/components/WelcomeBox/ImagePlaceHolder/ImagePlaceHolder.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ImagePlaceHolder } from './ImagePlaceHolder.component';
+
+export default {
+  title: 'ImagePlaceHolder',
+  component: ImagePlaceHolder,
+};
+export const PrimaryImage = () => <ImagePlaceHolder />;
