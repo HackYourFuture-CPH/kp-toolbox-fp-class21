91d04ecd82ab5fa5740afbeaa2f22bcc0c30ee17
vitalreddymynampati <vitalreddym@gmail.com>
2022-06-28 21:12:58 +0200

    FP21-17-footer updated footer as one component

--- packages/client/src/components/FooterSection/FooterSection.component.js
+++ packages/client/src/components/FooterSection/FooterSection.component.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './FooterSection.style.css';
-import { MainLogo } from '../MainLogo/MainLogo.component'
-import { SocialMediaLogo } from '../SocialMediaLogo/SocialMediaLogo.component';
-import { Disclaimer } from '../Disclaimer/Disclaimer.component';
+import { Link } from "react-router-dom";
+import vectorFacebook from '../../../public/assets/vectors/vectorFacebook.svg';
+import vectorInstagram from '../../../public/assets/vectors/vectorInstagram.svg';
+import vectorLinkedin from '../../../public/assets/vectors/vectorLinkedin.svg';
 
 export const FooterSection = ({ size }) => {
   return (
     <footer className="footerSection">
       <section className="footerContentSection">
-        <MainLogo />
-        <SocialMediaLogo />
-        <Disclaimer />
+      <section>
+      <Link to="/">
+        <p className="logo-text">KAOSPILOT</p>
+      </Link>
+    </section>
+    <section className="social-media-logo-container">
+      <Link to="/">
+        <img
+          className="social-media-logo"
+          src={vectorLinkedin}
+          alt="Linkedin-icon"
+        />
+      </Link>
+      <Link to="/">
+        <img
+          className="social-media-logo"
+          src={vectorInstagram}
+          alt="Instagram-icon"
+        />
+      </Link>
+      <Link to="/">
+        <img
+          className="social-media-logo"
+          src={vectorFacebook}
+          alt="Facebook-icon"
+        />
+      </Link>
+    </section>
+        <div className="placeholder-container">
+        <p className="placeholder-text">DISCLAIMER PLACEHOLDER</p>
+        </div>
       </section>
       <section className="footerCopyrightSection">
         <div className="footerCopyrightInfoSection">Copyright info...</div>
