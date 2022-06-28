290bf0baeb5fdda3c44282569970f407a243975b
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-06-28 10:41:40 +0200

    navbar container for responsive added,  nav links wrapper changed

--- packages/client/src/components/Navigation/Navbar.js
+++ packages/client/src/components/Navigation/Navbar.js
@@ -5,24 +5,18 @@ import './Navbar.css';
 export const Navbar = () => {
   return (
     <div className="navigation-bar">
-      <div className="logo">
-        <Link to="./">
-          <span>KAOSPILOT toolbox</span>
-        </Link>
-      </div>
-      <div className="navbar-links">
-        <Link to="/about-toolbox">
-          <li>about toolbox</li>
-        </Link>
-        <Link to="./contact-us">
-          <li>contact us</li>
-        </Link>
-        <Link to="./log-In">
-          <li>log In</li>
-        </Link>
-        <Link to="./favourites">
-          <li>favourites</li>
-        </Link>
+      <div className="navigation-bar-container">
+        <div className="logo">
+          <Link to="./">
+            <span>KAOSPILOT toolbox</span>
+          </Link>
+        </div>
+        <nav className="navbar-links">
+          <Link to="/about-toolbox">about toolbox</Link>
+          <Link to="/contact-us">contact us</Link>
+          <Link to="/log-In">log In</Link>
+          <Link to="/favourites">favourites</Link>
+        </nav>
       </div>
     </div>
   );
