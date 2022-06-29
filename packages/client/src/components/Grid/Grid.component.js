8ae5437f197b59b75b4c110a10a87e88936bc8bb
SvitlanaBurlaka <burlaka.sv.@gmail.com>
2022-06-29 17:10:36 +0200

    Added grid tools

---
+++ packages/client/src/components/Grid/Grid.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Grid.style.css';
+
+export const Grid = () => {
+  return (
+    <div className="grid-tools-container">
+      <div className="item">item</div>
+      <div className="item">item</div>
+      <div className="item">item</div>
+      <div className="item">item</div>
+      <div className="item">item</div>
+    </div>
+  );
+};
---
+++ packages/client/src/components/Grid/Grid.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid } from './Grid.component';
+
+export default {
+  title: 'Grid',
+  component: Grid.component,
+};
+
+export const Basic = () => <Grid />;
