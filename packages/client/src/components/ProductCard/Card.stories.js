73418931af0c876a462bf0f1dbfb837fec76cd76
vitalreddymynampati <vitalreddym@gmail.com>
2022-07-01 20:44:49 +0200

    grid-product-card

---
+++ packages/client/src/components/ProductCard/Card.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card } from './Card.component';
+
+export default {
+  title: 'Example/Card',
+  component: Card.component,
+};
+
+export const Basic = () => <Card />;
