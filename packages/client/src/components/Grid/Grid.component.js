3af9760c502073a323136d9007973010579e3df8
SvitlanaBurlaka <burlaka.sv.@gmail.com>
2022-07-01 20:58:53 +0200

    feature/FP21-24-subtask-grid added grid component wit responsive design

--- packages/client/src/components/Grid/Grid.component.js
+++ packages/client/src/components/Grid/Grid.component.js
@@ -2,13 +2,5 @@ import React from 'react';
 import './Grid.style.css';
 
 export const Grid = () => {
-  return (
-    <div className="grid-tools-container">
-      <div className="item">item</div>
-      <div className="item">item</div>
-      <div className="item">item</div>
-      <div className="item">item</div>
-      <div className="item">item</div>
-    </div>
-  );
+  return <div className="grid-tools-container" />;
 };
