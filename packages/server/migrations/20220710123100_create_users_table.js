525490834d1b97724a11c42b6311b56ed289b26e
rismita87 <rismita.ghosh@gmail.com>
2022-07-10 17:34:02 +0200

    created seeds and migrations for users and favourites tables

---
+++ packages/server/migrations/20220710123100_create_users_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('email').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
