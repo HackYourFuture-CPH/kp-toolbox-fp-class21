e75263b13a44b37feee1898a99903e7e4cee4908
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-07-19 15:59:28 +0200

    FP21-76:Add a ToolDetailsPage inside the PageNotFund & delete ToolDetailPage file

--- packages/client/src/containers/PageNotFound/PageNotFound.Container.js
+++ packages/client/src/containers/PageNotFound/PageNotFound.Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './PageNotFound.Style.css';
+import { ToolDetailsPage } from '../../components/ToolDetailsPage/ToolDetailsPage.component';
 
 export const PageNotFound = () => {
   return (
     <div className="page-not-found-container">
       <span>Page Not Found</span>
+      <ToolDetailsPage/>
     </div>
   );
 };
