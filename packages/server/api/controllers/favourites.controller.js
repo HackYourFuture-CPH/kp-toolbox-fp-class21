13c2510b58d28d8eeb1e30ea093f332f0a6c97bb
rebeccahockley <hockley.r@yahoo.com>
2022-08-02 15:18:55 +0200

    Updated endpoints

--- packages/server/api/controllers/favourites.controller.js
+++ packages/server/api/controllers/favourites.controller.js
@@ -1,29 +1,31 @@
 const knex = require('../../config/db');
 
-// const postFavorites = async (req, res) => {
-//   try {
-//     const newFavourite = await knex('favourites').insert(req.body);
-//     res.send(`Added new favourite: ${newFavourite}`);
-//   } catch (error) {
-//     res.status(400).send('Cannot add');
-//   }
-// };
-
 const postFavorites = async (body) => {
   const favourites = await knex('favourites').insert(body);
   return favourites;
 };
 
 const getAllFavourites = async () => {
-  const favourites = await knex('favourites').select('*');
+  const favourites = await knex('tools')
+    .select('*')
+    .join('favourites', 'favourites.tool_id', '=', 'tools.id')
+    .join('users', 'users.id', '=', 'favourites.user_id')
+    .join('tools_categories', 'tools_categories.tool_id', '=', 'tools.id')
+    .join('categories', 'tools_categories.category_id', '=', 'categories.id')
+    .where('users.id', '=', '3');
   return favourites;
 };
 
-const getFavouriteById = async () => {
-  const favourites = await knew('favourites').join();
+const deleteFavourites = async (toolId, userId, body) => {
+  const favourites = await knex('favourites')
+    .delete(body)
+    .where({ tool_id: toolId })
+    .andWhere({ user_id: userId });
+  return favourites;
 };
 
 module.exports = {
   postFavorites,
   getAllFavourites,
+  deleteFavourites,
 };
