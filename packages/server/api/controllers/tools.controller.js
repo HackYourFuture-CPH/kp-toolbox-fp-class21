12b7fb536a4ed2c27ac1c8ab8a1e2312c9c56418
MJanOthman <mhdoth97@gmail.com>
2022-08-03 21:10:11 +0200

    FP21-55-use-more-dynamic-class-naming-logic-for-streak-out-categories select categories using sql

--- packages/server/api/controllers/tools.controller.js
+++ packages/server/api/controllers/tools.controller.js
@@ -2,43 +2,30 @@ const knex = require('../../config/db');
 const HttpError = require('../lib/utils/http-error');
 
 const getTools = async () => {
-  let toolsWithCategories = [];
-  const tools = await knex('tools').select(
-    'id',
-    'name',
-    'time_frame_min',
-    'time_frame_max',
-    'group_size_min',
-    'group_size_max',
-    'facilitation_level',
-    'materials',
-    'pitch',
-    'description',
-    'instructions',
-    'source',
-    'picture',
-    'created_at',
-  );
-  const categoriesList = await knex('categories')
-    .select('categories.name', 'tools_categories.tool_id')
-    .join(
-      'tools_categories',
-      'tools_categories.category_id',
-      '=',
-      'categories.id',
-    );
-
-  toolsWithCategories = tools.map((tool) => {
-    const result = [];
-    categoriesList.map((category) => {
-      if (tool.id === category.tool_id) {
-        result.push(category.name);
-      }
-      return result;
-    });
-    return { ...tool, categories: result };
-  });
-  return toolsWithCategories;
+  // let toolsWithCategories = [];
+  const tools = await knex
+    .select(
+      'tools.id',
+      'tools.name',
+      'tools.time_frame_min',
+      'tools.time_frame_max',
+      'tools.group_size_min',
+      'tools.group_size_max',
+      'tools.facilitation_level',
+      'tools.materials',
+      'tools.pitch',
+      'tools.description',
+      'tools.instructions',
+      'tools.source',
+      'tools.picture',
+      'tools.created_at',
+      knex.raw('GROUP_CONCAT ( categories.name ) as categories'),
+    )
+    .from('tools')
+    .join('tools_categories', 'tools_categories.tool_id', '=', 'tools.id')
+    .join('categories', 'tools_categories.category_id', '=', 'categories.id')
+    .groupBy('tools.id');
+  return tools;
 };
 
 const getToolById = async (id) => {
