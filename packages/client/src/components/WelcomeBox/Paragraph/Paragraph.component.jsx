1f9150ed3b07cf84a16aa9f22361bf4a06062fa4
somayehdvr <somayehdvr@gmail.com>
2022-08-05 23:13:57 +0200

    prettier applied

--- packages/client/src/components/WelcomeBox/Paragraph/Paragraph.component.jsx
+++ packages/client/src/components/WelcomeBox/Paragraph/Paragraph.component.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const Paragraph = () => {
   return (
     <p className="welcome-box-paragraph">
-      Check out our collection of tools and methods for your team or organization!
-      Collaborate better, generate new ideas, create an inclusive space, 
-      dive into deep conversations & reflections …and more!
+      Check out our collection of tools and methods for your team or
+      organization! Collaborate better, generate new ideas, create an inclusive
+      space, dive into deep conversations & reflections …and more!
     </p>
   );
 };
