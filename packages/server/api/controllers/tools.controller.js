38152cc32ac9e979dce19d676849a7a6ec566710
somayehdvr <somayehdvr@gmail.com>
2022-08-08 00:18:08 +0200

    merge branch develop

--- packages/server/api/controllers/tools.controller.js
+++ packages/server/api/controllers/tools.controller.js
@@ -2,22 +2,23 @@ const knex = require('../../config/db');
 const HttpError = require('../lib/utils/http-error');
 
 const getTools = async (query) => {
-  let tools = knex('tools').select(
-    'tools.id',
-    'tools.name',
-    'tools.time_frame_min',
-    'tools.time_frame_max',
-    'tools.group_size_min',
-    'tools.group_size_max',
-    'tools.facilitation_level',
-    'tools.materials',
-    'tools.pitch',
-    'tools.description',
-    'tools.instructions',
-    'tools.source',
-    'tools.picture',
-    'tools.created_at',
-    knex.raw('GROUP_CONCAT ( categories.name ) as categories'),
+  let tools = knex
+    .select(
+      'tools.id',
+      'tools.name',
+      'tools.time_frame_min',
+      'tools.time_frame_max',
+      'tools.group_size_min',
+      'tools.group_size_max',
+      'tools.facilitation_level',
+      'tools.materials',
+      'tools.pitch',
+      'tools.description',
+      'tools.instructions',
+      'tools.source',
+      'tools.picture',
+      'tools.created_at',
+      knex.raw('GROUP_CONCAT ( categories.name ) as categories'),
     )
     .from('tools')
     .join('tools_categories', 'tools_categories.tool_id', '=', 'tools.id')
@@ -25,19 +26,17 @@ const getTools = async (query) => {
     .groupBy('tools.id');
 
   if ('category' in query) {
-    tools = tools
-      .join('tools_categories', 'tools_categories.tool_Id', '=', 'tools.id')
-      .where((orBuilder) =>
-        query.category.forEach((item) => {
-          if (!Number(parseInt(item, 10))) {
-            throw new HttpError(
-              'please specify the IDs of the categories that you want to select. They should be numbers. ',
-              400,
-            );
-          }
-          orBuilder.orWhere('tools_categories.category_Id', '=', item);
-        }),
-      );
+    tools = tools.where((orBuilder) =>
+      query.category.forEach((item) => {
+        if (!Number(parseInt(item, 10))) {
+          throw new HttpError(
+            'please specify the IDs of the categories that you want to select. They should be numbers. ',
+            400,
+          );
+        }
+        orBuilder.orWhere('tools_categories.category_id', '=', item);
+      }),
+    );
   }
 
   if ('timeframe' in query) {
