4002b4ed2ddabf06ad73e8d87b7030654a2c1ba9
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-08-09 01:17:49 +0200

    FP21-89: add kp image

--- packages/client/src/components/Main/Main.js
+++ packages/client/src/components/Main/Main.js
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Main.css';
 
-export const Main = ({ children }) => {
+export const Main = ({ children, ...props }) => {
   return (
     <main>
-      <div className="main-container">{children}</div>
+      <div className="main-container">
+        {children}
+        <img
+          className="kpImg"
+          src="/assets/images/kp.png"
+          alt=""
+          width="1219.88px"
+          height="597.86px"
+        />
+      </div>
     </main>
   );
 };
