7142e2693914e39cb40e7b80a98315f209c84db3
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-08-08 21:35:58 +0200

    FP21-84:I created stories file to loader

---
+++ packages/client/src/components/Loader/Loader.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Loader } from './Loader.component';
+
+export default {
+  title: 'Loader',
+  component: Loader.component,
+};
+
+export const Basic = () => <Loader />;
