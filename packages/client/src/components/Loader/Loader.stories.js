7dafb5f18a1c59ba180c40a8800ede88f5e0181d
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-08-08 21:46:27 +0200

    FP21-84:I delete Loader.stories i gusse unnecessary

--- packages/client/src/components/Loader/Loader.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Loader } from './Loader.component';
-
-export default {
-  title: 'Loader',
-  component: Loader.component,
-};
-
-export const Basic = () => <Loader />;
