e32fcb172a84c4d1e5199e35f3364c9cf95ee940
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-08-08 20:49:00 +0200

    FP21-88:add a image and linked to Breadcrumbs a link to Home Page

--- packages/client/src/components/ToolDetailsPage/ToolDetailsPage.component.js
+++ packages/client/src/components/ToolDetailsPage/ToolDetailsPage.component.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import getApiBaseUrl from '../../utils/getApiBaseURL';
 import './ToolDetailsPage.css';
 
@@ -60,9 +60,11 @@ export const ToolDetailsPage = () => {
 
     pageContent = (
       <>
-        <p className="breadcrumbs">
-          KAOSPILOT toolbox / <span>{tool.name}</span>
-        </p>
+        
+          <p className="breadcrumbs">
+          <Link to='/'> KAOSPILOT toolbox / <span>{tool.name}</span></Link>
+           
+          </p>
         <h1>{tool.name}</h1>
         <img
           className="tool-image"
@@ -92,6 +94,12 @@ export const ToolDetailsPage = () => {
         <div className="tool-description">
           <p>{tool.pitch}</p>
           <p>{tool.description}</p>
+          <p />
+          <img
+            src="/assets/images/Kaospilot_admissionworkshop9_2016_BW 1.png"
+            width="1003.65px"
+            height="300px"
+          />
         </div>
 
         <div className="tool-instructions">
