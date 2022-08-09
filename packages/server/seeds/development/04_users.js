fd47e0dbc0ce6b2ef21a4b09fa54474b9434f223
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-08-09 11:13:24 +0200

    users endpoints added, migrations and seed fixed, user check function added while siging in

--- packages/server/seeds/development/04_users.js
+++ packages/server/seeds/development/04_users.js
@@ -6,8 +6,20 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del();
   await knex('users').insert([
-    { first_name: 'user1', last_name: 'lastname1', email: 'user1@gmail.com' },
-    { first_name: 'user2', last_name: 'lastname2', email: 'user2@gmail.com' },
-    { first_name: 'user3', last_name: 'lastname3', email: 'user3@gmail.com' },
+    {
+      name: 'user1',
+      email: 'user1@gmail.com',
+      firebase_id: 'testId1',
+    },
+    {
+      name: 'user2',
+      email: 'user2@gmail.com',
+      firebase_id: 'testId2',
+    },
+    {
+      name: 'user3',
+      email: 'user3@gmail.com',
+      firebase_id: 'testId3',
+    },
   ]);
 };
