9a630f6fe928de88845ea39d920bfa1997d8c610
somayehdvr <somayehdvr@gmail.com>
2022-08-21 14:41:10 +0200

    prettier applied

--- packages/server/migrations/20220821123507_add_users_table_column.js
+++ packages/server/migrations/20220821123507_add_users_table_column.js
@@ -2,19 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up = function (knex) {
-    return knex.schema.table('users', (table) => {
-      table.boolean('is_admin').defaultTo(false);
-    });
-  };
-  
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  exports.down = function (knex) {
-    return knex.schema.table('users', (table) => {
-      table.dropColumn('is_admin');
-    });
-  };
-  
+exports.up = function (knex) {
+  return knex.schema.table('users', (table) => {
+    table.boolean('is_admin').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('users', (table) => {
+    table.dropColumn('is_admin');
+  });
+};
