c4fa8d34d6cbed3d8c63e30ba84dfaeedb5720b9
oleksandra-lavryk <oleksandra.lavryk96@gmail.com>
2022-08-21 14:46:03 +0200

    fixed scroll to top when link to another page

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
@@ -13,24 +18,34 @@ import { Inbox } from './components/InboxMessageAdmin/Inbox.component';
 import { AuthContextProvider } from './firebase/AuthContext';
 import { AboutToolbox } from './components/AboutToolbox/AboutToolbox.component';
 
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
+
 function App() {
   return (
     <div className="app">
       <Router>
         <AuthContextProvider>
           <Navbar />
-          <Main>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="*" element={<PageNotFound />} />
-              <Route path="/tools/:id" element={<ToolDetailsPage />} />
-              <Route path="/about-toolbox" element={<AboutToolbox />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route path="/user-name" element="" />
-              <Route path="/favourites" element={<FavouritePage />} />
-              <Route path="/inbox-admin" element={<Inbox />} />
-            </Routes>
-          </Main>
+          <Wrapper>
+            <Main>
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/tools/:id" element={<ToolDetailsPage />} />
+                <Route path="/about-toolbox" element={<AboutToolbox />} />
+                <Route path="/contact-us" element={<ContactUs />} />
+                <Route path="/user-name" element="" />
+                <Route path="/favourites" element={<FavouritePage />} />
+                <Route path="/inbox-admin" element={<Inbox />} />
+              </Routes>
+            </Main>
+          </Wrapper>
           <Footer />
         </AuthContextProvider>
       </Router>
